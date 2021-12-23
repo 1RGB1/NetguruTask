9395a9a5da6fa2de99b58f42a1ecee02c6f8b27c
Ahmad Ragab <a.ragab2012@gmail.com>
2021-12-23 19:31:20 +0200

    Update cr.swift
    Fixing code review points

--- cr.swift
+++ cr.swift
@@ -3,12 +3,47 @@
 //
 
 class paymentViewController: UIViewController {
-    var delegate: PaymentViewControllerDelegate
+    
+    weak var delegate: PaymentViewControllerDelegate?
     let customView = PaymentView()
     let payment: Payment?
+    internal let viewModel: PaymentViewModel?
 
-    func callbacks() {
+    func viewDidLoad() {
+        navigationController?.setNavigationBarHidden(false, animated: false)
+        customView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        customView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        view.addSubview(customView)
+        updateBackground()
+        fetchPayment()
+        callbacks()
+    }
+    
+    func setupViewModel(viewModel: PaymentViewModel) {
+        self.viewModel = viewModel
+    }
+    
+    func updateBackground() {
         view.backgroundColor = UIColor(displayP3Red: 1, green: 1, blue: 1, alpha: 1)
+    }
+
+    func fetchPayment() {
+        guard let viewModel = viewModel else { return }
+        
+        viewModel.fetchPayment()
+
+//        customView.statusText = "Fetching data"
+//        ApiClient.sharedInstance().fetchPayment { [weak self] payment in
+//            guard let self = self else { return }
+//            self.customView.isEuro = (payment?.currency ?? "") == "EUR" ? true : false
+//            if (payment?.amount ?? 0) != 0 {
+//                self.updatePaymentAmount(amountStr: "\(payment?.amount ?? 0)")
+//                return
+//            }
+//        }
+    }
+    
+    func callbacks() {
         customView.didTapButton = {
             if let payment = self.payment {
                 self.delegate.didFinishFlow(amount: payment.amount
@@ -16,26 +51,10 @@ class paymentViewController: UIViewController {
             }
         }
     }
-
-    func viewDidLoad() {
-        navigationController?.setNavigationBarHidden(false, animated: false)
-        customView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
-        customView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-        view.addSubview(customView)
-        fetchPayment()
-        setupCallbacks()
-    }
-
-    internal let ViewModel: PaymentViewModel
-
-    func fetchPayment() {
-        customView.statusText = "Fetching data"
-        ApiClient.sharedInstance().fetchPayment { payment in
-            self.CustomView.isEuro = payment.currency == "EUR" ? true : false
-            if payment!.amount != 0 {
-                self.CustomView.label.text = "\(payment!.amount)"
-                return
-            }
+    
+    func updatePaymentAmount(amountStr: String) {
+        DispatchQueue.main.async {
+            self.customView.updateTitleLabel(amountStr)
         }
     }
 }
